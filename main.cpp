2b31df68688ea116ee6ff40cb513fe80214202bf
hyrum-c <hyrum.cox@gmail.com>
2024-02-05 17:02:50 -0700

    Started on noteutils

---
+++ main.cpp
@@ -0,0 +1,11 @@
+/* 
+ * Simple program to test making sounds using the Windows.h header
+ *
+ */
+
+#include "windows.h"
+
+int main() {
+    Beep(50000, 500);
+    return 0;
+}
---
+++ noteutil.h
@@ -0,0 +1,11 @@
+/*
+ * Contains information relating to making music
+ * with the Beep function in windows.h
+ */
+
+enum keys {
+    A1 = 55,
+    A2 = 110,
+    A3 = 220,
+    A4 = 440,
+};
